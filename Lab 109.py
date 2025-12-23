b258858bc7de801eba2682832f4b8108ccec0989
kalaivanan-pk <pkalaivanan210@gmail.com>
2025-12-23 12:06:15 +0530

    Update Lab 109.py
    new variable change 5 to 6

--- Lab 109.py
+++ Lab 109.py
@@ -7,7 +7,8 @@ myvalue=3.14
 print(myvalue)
 print(type(myvalue))
 print(str(myvalue)+" is of the data type "+str(type(myvalue)))
-myvalue=5j
+
+myvalue=6j
 print(myvalue)
 print(type(myvalue))
 print(str(myvalue)+" is of the data type "+str(type(myvalue)))
